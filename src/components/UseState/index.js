d0af93b954d35d99b36bfadb9928214aeedd372c
zearkiatos <caprilespe@outlook.com>
2021-12-05 22:36:09 -0300

    Effects with UseEffect and add github action configuration

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+
+const FAKE_LOADING_TIMER = 3000;
 
 const UseState = ({ name }) => {
-    const [error, setError] = useState(false);
-    const onClickHandle = () => {
-        setError(!error);
-    };
-    return (
-        <div>
-            <h2>
-                Delete {name}
-            </h2>
-            <p>Please, write the security code.</p>
-            {error && (
-                <p>The code was wrong</p>
-            )}
-            <input placeholder="Security Code" />
-            <button
-                onClick={onClickHandle}
-            >
-                Check
-            </button>
-        </div>
-    );
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const onClickHandle = () => {
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    console.log("Begin the Effect");
+    loading &&
+      setTimeout(() => {
+        console.log("Making the validation");
+        setLoading(false);
+        console.log("Ending the validation");
+      }, FAKE_LOADING_TIMER);
+    console.log("Ending the Effect");
+  }, [loading]);
+  return (
+    <div>
+      <h2>Delete {name}</h2>
+      <p>Please, write the security code.</p>
+      {error && <p>The code was wrong</p>}
+      {loading && <p>Loading...</p>}
+      <input placeholder="Security Code" />
+      <button onClick={onClickHandle}>Check</button>
+    </div>
+  );
 };
 
 export default UseState;

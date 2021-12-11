a9ca717751b60a5237160bab63add79b9f5e1344
zearkiatos <caprilespe@outlook.com>
2021-12-10 21:36:00 -0300

    Compose states with React.Component

--- src/components/ClassState/index.js
+++ src/components/ClassState/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Loading from "../Loading";
+import config from "../../config";
 
 class ClassState extends Component {
   FAKE_LOADING_TIMER = 3000;
@@ -7,37 +8,55 @@ class ClassState extends Component {
     super(props);
     this.state = {
       error: false,
-      loading: false
+      loading: false,
+      value: ""
     };
   }
 
-  onClickHandler = (error) => {
+  onClickHandler = () => {
     this.setState({
-      error: !error,
       loading: true
     });
   };
 
+  onChangeHandler = (event) => {
+    this.setState({
+      error: false,
+      value: event.target.value
+    });
+  };
+
   componentDidUpdate() {
-    const { loading } = this.state;
+    const { loading, value } = this.state;
     loading &&
       setTimeout(() => {
-        this.setState({
+        if (config.SECURITY_CODE === value) {
+          this.setState({
             loading: false
-        })
+          });
+        } else {
+          this.setState({
+            error: true,
+            loading: false
+          });
+        }
       }, this.FAKE_LOADING_TIMER);
   }
   render() {
     const { name } = this.props;
-    const { error, loading } = this.state;
+    const { error, loading, value } = this.state;
     return (
       <div>
         <h2>Delete {name}</h2>
         <p>Please, write the security code.</p>
         {error && <p>The code was wrong</p>}
         {loading && <Loading />}
-        <input placeholder="Security Code" />
-        <button onClick={() => this.onClickHandler(error)}>Check</button>
+        <input
+          placeholder="Security Code"
+          value={value}
+          onChange={this.onChangeHandler}
+        />
+        <button onClick={this.onClickHandler}>Check</button>
       </div>
     );
   }
